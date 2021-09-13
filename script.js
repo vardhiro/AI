d82ecdb8bd6eca8064fe57654fcefa79f7a80121
NanCoder900 <nandishnana@gmail.com>
2021-09-13 23:17:53 +0530

    Change in form

--- script.js
+++ script.js
@@ -718,3 +718,6 @@ greetingDOM.innerText = greeting;
 if (localStorage.getItem("First Name"))
 document.title = `AI - ${greeting} ${localStorage.getItem("First Name")}!`;
 // 626ae011b9f2a70efc5fc4f98b510fe4
+
+document.getElementById("user_name").value = localStorage.getItem("First Name");
+document.getElementById("user_email").value = localStorage.getItem("Email");
